d688edfa199a0981f05330e7558cc9c9cad45db9
AbdoSoliman1 <pyramidfourth40@gmail.com>
2026-03-20 02:11:56 +0200

    Update SoundService to use VideoPlayer
    Replaced SoundPlayer with VideoPlayer and adjusted logic to play videos instead of sounds.

--- sound_service.py
+++ sound_service.py
@@ -1,33 +1,25 @@
 import threading
 import os
-from sound_player import SoundPlayer
+from video_player import VideoPlayer
 from notification_manager import NotificationManager
 
 class SoundService:
-    def __init__(self, folder_path, sound_gif_map, interval_minutes=10):
-        self.player = SoundPlayer(folder_path)
+    def __init__(self, folder_path, interval_minutes=10):
+        self.player = VideoPlayer(folder_path)
         self.notification = NotificationManager()
-        self.sound_gif_map = sound_gif_map
         self.interval_seconds = interval_minutes * 60
         self.timer = None
 
     def start(self):
-        sound_path = self.player.pick_random_sound()
+        video_path = self.player.pick_random_video()
 
-        if sound_path:
-            sound_name = os.path.basename(sound_path)
-            gif_path = self.sound_gif_map.get(sound_name, None)
-            print("Playing: " + sound_name)
-
-            self.notification.show(sound_name, gif_path)
-        
-            import time
-            time.sleep(1)
-        
-            self.player.play(sound_path)
+        if video_path:
+            video_name = os.path.basename(video_path)
+            print("Playing: " + video_name)
+            self.notification.show(video_path)
 
         self._schedule_next()
-        
+
     def _schedule_next(self):
         self.timer = threading.Timer(self.interval_seconds, self.start)
         self.timer.daemon = True
@@ -37,3 +29,4 @@ class SoundService:
         if self.timer is not None:
             self.timer.cancel()
         print("Service stopped.")
+    
