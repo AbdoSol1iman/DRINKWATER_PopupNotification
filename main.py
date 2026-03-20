15e8edacceba4aa15ed0db21571616ebf11f911d
AbdoSoliman1 <pyramidfourth40@gmail.com>
2026-03-20 02:11:29 +0200

    Refactor SoundService initialization with videos folder

--- main.py
+++ main.py
@@ -5,19 +5,9 @@ import time
 from sound_service import SoundService
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+VIDEOS_FOLDER = os.path.join(BASE_DIR, "videos")
 
-SOUNDS_FOLDER = os.path.join(BASE_DIR, "sounds")
-
-SOUND_GIF_MAP = {
-    "Facebook(1).wav" : os.path.join(BASE_DIR, "assets", "Facebook(1).gif"),
-    "Facebook(2).wav" : os.path.join(BASE_DIR, "assets", "Facebook(2).gif"),
-    "Facebook(4).wav" : os.path.join(BASE_DIR, "assets", "Facebook(4).gif"),
-    "Facebook(5).wav" : os.path.join(BASE_DIR, "assets", "Facebook(5).gif"),
-    "Facebook(6).wav" : os.path.join(BASE_DIR, "assets", "Facebook(6).gif"),
-    "Facebook(7).wav" : os.path.join(BASE_DIR, "assets", "Facebook(7).gif"),
-}
-
-service = SoundService(SOUNDS_FOLDER, SOUND_GIF_MAP, interval_minutes=10)
+service = SoundService(VIDEOS_FOLDER, interval_minutes=10)
 
 def handle_stop(sig, frame):
     service.stop()
