8a485b210277fecf5541a9e60947c8938e68b6e4
AbdoSoliman1 <pyramidfourth40@gmail.com>
2026-03-20 02:12:54 +0200

    Replace GIF display with video playback

--- notification_manager.py
+++ notification_manager.py
@@ -1,62 +1,57 @@
 import tkinter as tk
-from PIL import Image, ImageTk
+import vlc
 import threading
 
 class NotificationManager:
     def __init__(self):
         pass
 
-    def show(self, sound_file_name, gif_path):
+    def show(self, video_file):
         ready = threading.Event()
-        thread = threading.Thread(target=self._show_popup, args=(sound_file_name, gif_path, ready))
+        thread = threading.Thread(target=self._show_popup, args=(video_file, ready))
         thread.daemon = True
         thread.start()
         ready.wait()
 
-    def _show_popup(self, sound_file_name, gif_path, ready):
-        if gif_path is None:
-            print("No GIF mapped for: " + sound_file_name)
+    def _show_popup(self, video_file, ready):
+        if video_file is None:
+            print("No video file provided")
             ready.set()
             return
 
         root = tk.Tk()
         root.overrideredirect(True)
         root.attributes('-alpha', 0.0)
-        root.configure(bg='#1e1e1e')
+        root.configure(bg='black')
 
         POPUP_W = 420
         POPUP_H = 300
 
-        gif = Image.open(gif_path)
-        frames = []
-        try:
-            while True:
-                frame = gif.copy().resize((400, 300))
-                frames.append(ImageTk.PhotoImage(frame))
-                gif.seek(gif.tell() + 1)
-        except EOFError:
-            pass
-
-        gif_label = tk.Label(root, bg='#1e1e1e')
-        gif_label.pack(padx=10, pady=(10, 5))
-
-
-        # Position at bottom-right
         root.update_idletasks()
         screen_w = root.winfo_screenwidth()
         screen_h = root.winfo_screenheight()
-        win_w = root.winfo_width()
-        win_h = root.winfo_height()
         x = screen_w - POPUP_W - 20
         y = screen_h - POPUP_H - 50
         root.geometry(f'{POPUP_W}x{POPUP_H}+{x}+{y}')
-        
-        self._animate_gif(gif_label, frames, 0)
+
+        video_frame = tk.Frame(root, bg='black', width=POPUP_W, height=POPUP_H)
+        video_frame.pack()
+
+        instance = vlc.Instance()
+        player = instance.media_player_new()
+        media = instance.media_new(video_file)
+        player.set_media(media)
+
+        root.update()
+        player.set_xwindow(video_frame.winfo_id()) 
+
+        player.play()
 
         ready.set()
 
         self._fade_in(root)
-        root.after(10000, lambda: self._fade_out(root))
+
+        root.after(10000, lambda: self._fade_out(root, player))
 
         root.mainloop()
 
@@ -67,11 +62,12 @@ class NotificationManager:
         else:
             window.attributes('-alpha', 1.0)
 
-    def _fade_out(self, window, opacity=1.0):
+    def _fade_out(self, window, player, opacity=1.0):
         if opacity > 0.0:
             window.attributes('-alpha', opacity)
-            window.after(30, lambda: self._fade_out(window, round(opacity - 0.1, 1)))
+            window.after(30, lambda: self._fade_out(window, player, round(opacity - 0.1, 1)))
         else:
+            player.stop()
             window.destroy()
 
     def _animate_gif(self, label, frames, index):
